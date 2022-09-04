1945e5c7b64ca4110f50c75cf8834afd612a0ab9
Zaqueu Santos <zaqueulima1@gmail.com>
2022-09-04 19:41:06 -0300

    feat: :sparkles: create loading component

---
+++ moviefinder/src/main/java/dev/zaqueu/moviefinder/presentation/components/Loading.kt
@@ -0,0 +1,21 @@
+package dev.zaqueu.moviefinder.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Loading(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
